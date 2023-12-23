2c6b00375728e4f78b17e03f4c7daa279b476bb1
unblocktorrents <76567597+unblocktorrents@users.noreply.github.com>
2023-12-24 02:11:33 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,6 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
 
 html_build_dir = os.environ.get('READTHEDOCS_OUTPUT', 'docs/build/html')
